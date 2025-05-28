1f764c12c86993620a7aeea1164610c78dc548b5
devkbm <devkbm0417@gmail.com>
2025-05-28 21:53:07 +0900

    2025-05-28

--- src/main/java/com/like/ConnectBoot35Application.java
+++ src/main/java/com/like/ConnectBoot35Application.java
@@ -1,8 +1,14 @@
 package com.like;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+import jakarta.annotation.PostConstruct;
+
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class ConnectBoot35Application {
 
@@ -10,4 +16,8 @@ public class ConnectBoot35Application {
 		SpringApplication.run(ConnectBoot35Application.class, args);
 	}
 
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 }
